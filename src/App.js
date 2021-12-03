3e5e625dd16b92eaa452f37c24b715cdff09a9e4
Ronak98 <sonironak0906@gmail.com>
2021-12-03 21:05:54 +0530

    created components for sign in & shop page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "./App.css";
-import HomePage from "./pages/homepage/homepage.component";
 import { Switch, Route } from "react-router-dom";
-import ShopPage from "./pages/shop/shop.component";
+
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shoppage/shop.component";
+import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-uppage/sign-in-and-sign-up.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
